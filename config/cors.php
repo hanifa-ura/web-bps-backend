9dc4f9add985c2ae60d8c7b266e7d1af26ec8a31
hanifa-ura <hanifaaura5008092@gmail.com>
2025-07-13 17:16:23 +0700

    Edit cors.php agar konek ke frontend

--- config/cors.php
+++ config/cors.php
@@ -1,10 +1,12 @@
 <?php
+
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'https://web-bps-frontend-eight.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
+
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
