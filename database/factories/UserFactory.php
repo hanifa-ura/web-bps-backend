367d424f4f00fe37ed0ff5f6d9c40b9356bc1a35
hanifa-ura <hanifaaura5008092@gmail.com>
2025-07-13 23:27:37 +0700

    edit userFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,13 +21,13 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
+    public function definition()
     {
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => '$2y$12$faTwV/cFvsJVBa.ImsSvC.LBpUXMEE84RJD1h604V47i2kM30rGLi',
             'remember_token' => Str::random(10),
         ];
     }
